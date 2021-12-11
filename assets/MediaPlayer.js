fd875ce3c2c25eff4e7c59fa276ceee158fc95c0
Jonathan Felipe Hernández Catalán <jfhernandez@ceroble.edu.gt>
2021-12-11 07:36:25 -0600

    Learning plugins

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,22 +1,38 @@
+function MediaPlayer(config) {
+  this.media = config.el;
+  this.plugins = config.plugins || [];
 
-      function MediaPlayer(config){
-        this.media = config.el;
-      }
+  this._initPlugins();
+}
 
-      MediaPlayer.prototype.play = function(){
-        this.media.play();
-      }
+MediaPlayer.prototype._initPlugins = function() {
+  this.plugins.forEach(plugin => {
+    plugin.run(this);
+  });
+};
 
-      MediaPlayer.prototype.pause = function(){
-        this.media.pause();
-      }
- 
-      MediaPlayer.prototype.togglePlay = function(){
-        if (this.media.paused) {
-          this.play();
-        } else {
-          this.pause();
-        }
-      }
+MediaPlayer.prototype.play = function() {
+  this.media.play();
+};
 
-      export default MediaPlayer;
+MediaPlayer.prototype.pause = function() {
+  this.media.pause();
+};
+
+MediaPlayer.prototype.togglePlay = function() {
+  if (this.media.paused) {
+    this.play();
+  } else {
+    this.pause();
+  }
+};
+
+MediaPlayer.prototype.mute = function() {
+  this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function() {
+  this.media.muted = false;
+};
+
+export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,22 @@
       import MediaPlayer from './MediaPlayer.js';
+      import AutoPlay from './plugins/AutoPlay.js'; 
 
       const video = document.querySelector("video");
 
-      const player = new MediaPlayer( { el: video } );
+      const player = new MediaPlayer( { el: video, plugins:[
+
+      ] } );
       
-      const button = document.querySelector("button");
+      const button = document.querySelector("#playButton");
       button.onclick = () => player.togglePlay();
+
+      const buttonMute = document.querySelector("#muteButton");
+      buttonMute.onclick = () => {
+            if(player.media.muted){
+                  player.unmute();
+            }else {
+                  player.mute();
+            }
+      };
+
+
